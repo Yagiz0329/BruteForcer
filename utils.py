230976f6fc6e5e5277ba1c2e0a8386d647540c89
Yağız Yedier <165019526+Yagiz0329@users.noreply.github.com>
2025-06-05 12:46:24 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,12 @@
-def log_success(msg):
-    print("\033[92m" + msg + "\033[0m")
+# utils.py
+from datetime import datetime
 
-def log_fail(msg):
-    print("\033[91m" + msg + "\033[0m")
+def log_success(service, username, password, target):
+    with open(f"{service}_success.log", "a") as log:
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        log.write(f"[{timestamp}] {username}:{password} @ {target}\n")
+
+def print_banner():
+    print("="*50)
+    print("     🔐 BruteForcer Tool by Yagiz Yedier")
+    print("="*50)
